bc0d8f902d58708c3d12966359ea6b1f223cd80a
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-08-21 12:39:02 -0600

    update

--- app.mjs
+++ app.mjs
@@ -21,7 +21,8 @@ const allowedOrigins = [
     'http://localhost:3001', 
     'http://localhost:5173', 
     'http://localhost:5174',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://172.30.18.135'
 ];
 
 app.use(cors({
